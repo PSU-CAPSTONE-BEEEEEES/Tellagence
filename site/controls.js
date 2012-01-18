ff90f2f947706deb9711cbbe0185dcc09b63a819
aren <aren@pdx.edu>
2012-01-18 15:27:40 -0800

    Added controls.js and hooked hello button to script

---
+++ site/controls.js
@@ -0,0 +1,20 @@
+/*
+Author: Aren Edlund-Jermain
+The redraw button function is linked to the redraw button in the control panel.
+It will eventually redraw the image. I think.
+*/
+function redrawButton()
+{
+    alert("Hello World!");
+}
+
+/*
+Author: Aren Edlund-Jermain
+The zoomSlider function is linked to the zoom slider in the control panel. It
+will redraw the render with different sizing. Making the object smaller or
+larger depending upon the value retrieved from the actual slider.
+*/
+function zoomSlider()
+{
+    
+}
