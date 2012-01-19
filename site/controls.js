7e1dd5145d42eba85623785dc18dfbe27d089696
Derek Hurley <dzhurley@gmail.com>
2012-01-19 01:49:03 -0800

    started #956, buttons trigger on mousedown until mouseup
    behavior to continuously fire an event handler on mousedown
    and stop on mouseup could happen due to setInterval, and
    the behavior demos itself in the input text field using the
    up and down buttons
    
    the html5 was adjust to give id attributes to the different
    buttons for the jQuery selectors

--- site/controls.js
+++ site/controls.js
@@ -1,20 +1,68 @@
-/*
-Author: Aren Edlund-Jermain
-The redraw button function is linked to the redraw button in the control panel.
-It will eventually redraw the image. I think.
-*/
-function redrawButton()
-{
-    alert("Hello World!");
-}
+$(document).ready(function() {
+    // used to save the interval handle
+    var i;
 
-/*
-Author: Aren Edlund-Jermain
-The zoomSlider function is linked to the zoom slider in the control panel. It
-will redraw the render with different sizing. Making the object smaller or
-larger depending upon the value retrieved from the actual slider.
-*/
-function zoomSlider()
-{
-    
-}
+    // Keeps track of times the functions in setInterval are called. This is
+    // offset to 1 for the .join() operation in search_bar.
+    var num_called = 1;
+
+    /*
+      These two functions select the up and down buttons on mousedown events and
+      repeat a function at the given interval (100 ms). The repetition will
+      continue until mouseup or mouseleave events occur.
+     */
+    $("#pan_up").mousedown(function() {
+	i = setInterval(function() {search_bar("up");}, 100);
+    }).bind("mouseup mouseleave", function() {
+	clearInterval(i);
+    });
+
+    $("#pan_down").mousedown(function() {
+	i = setInterval(function() {search_bar("down");}, 100);
+    }).bind("mouseup mouseleave", function() {
+	clearInterval(i);
+    });
+
+    /*
+      Author: Aren Edlund-Jermain
+      The redraw button function is linked to the redraw button in the control
+      panel. It will eventually redraw the image. I think.
+    */
+    function redrawButton()
+    {
+	alert("Hello World!");
+    }
+
+    /*
+      Author: Aren Edlund-Jermain
+      The zoomSlider function is linked to the zoom slider in the control panel. It
+      will redraw the render with different sizing. Making the object smaller or
+      larger depending upon the value retrieved from the actual slider.
+    */
+    function zoomSlider()
+    {
+	
+    }
+
+    /*
+      search_bar is a demo function that increases/decreases the number of '|'
+      in the text input field on up/down button presses. It is purely to show
+      how holding the buttons can trigger functions continuously until the
+      button is released.
+     */
+    function search_bar(type)
+    {
+	if (type == "up") {
+	    num_called += 1;
+	}
+	else if (type == "down"){
+	    num_called -= 1;
+	}
+
+	// creates an Array of '|' at length num_called - 1 and sets it in the
+	// input text field
+	var text = Array(num_called).join("|")
+	$("#search").attr("value", text);
+    };
+
+});
--- site/test.js
+++ site/test.js
@@ -1,17 +1,19 @@
 /* Example script showing svg dimension selection and resize */
 
-// select section width to get offset from toolbar, keep window height
-var w = $("#d3").width();
-var h = $(window).height();
-
-// draw the svg in the d3 section
-var svg = d3.select("#d3").append("svg")
-    .attr("width", w)
-    .attr("height", h);
-
-// reset the svg with the new width and height after window resize
-$(window).resize(function() {
+$(document).ready(function() {
+    // select section width to get offset from toolbar, keep window height
     var w = $("#d3").width();
     var h = $(window).height();
-    svg.attr("width", w).attr("height", h);
+
+    // draw the svg in the d3 section
+    var svg = d3.select("#d3").append("svg")
+	.attr("width", w)
+	.attr("height", h);
+
+    // reset the svg with the new width and height after window resize
+    $(window).resize(function() {
+	var w = $("#d3").width();
+	var h = $(window).height();
+	svg.attr("width", w).attr("height", h);
+    });
 });
