572251df49576e57ee25acf019354cb98f2ccf9b
longuyen <longuyen@cecs.pdx.edu>
2012-01-18 22:10:12 -0800

    add randrom2.php -> research/2d_graph/

---
+++ research/2d_graph/random2.php
@@ -0,0 +1,79 @@
+<?php
+
+//determine graph size & shape
+
+$count = 20;
+if (isset($_GET["size"]))
+{
+  $size = $_GET["size"];
+  if ( (int)$size == $size)
+  {
+    $count = (int)$size;
+  }
+}
+
+
+$sparse = 3;
+if (isset($_GET["factor"]))
+{
+  $factor = $_GET["factor"];
+  if ( (int)$factor == $factor && $factor >= 0)
+  {
+    $sparse = $factor;
+  }
+}
+
+//determine min and max influences
+
+$min = 0;
+if (isset($_GET["min"]) && ((int)$_GET["min"] == $_GET["min"]) )
+{
+    $min = $_GET["min"];
+}
+
+$max = $count;
+if (isset($_GET["max"]) && ((int)$_GET["max"] == $_GET["max"]) )
+{
+    $max = $_GET["max"];
+}
+
+//begin JSON generation
+function genName($index)
+{
+    $letters = range('a', 'z');
+    $result = "";
+    $q = floor($index / 26);
+    $r = $index - ($q * 26);
+    $result .= $letters[$r];
+    while ($q >= 26)
+    {
+        $index /= 26;
+        $q = floor($index / 26);
+        $r = $index - ($q * 26);
+        $result .= $letters[$r];
+    }
+
+    return $result;
+}
+
+$json['nodes'] = array();
+for ($i = 0; $i <= $count; $i++) {
+    $node['name'] = genName($i);
+    $json['nodes'][] = $node;
+}
+
+$json['links'] = array();
+for ($i = 0; $i <= $count; $i++) {
+    for ($j = $i-1; $j > 0; $j--) {
+        if ( rand(0,$sparse) == 0 ) {
+            $link['source'] = $i;
+            $link['target'] = $j;
+            $link['influence'] = rand($min,$max);
+	    $json['links'][] = $link;
+        }
+    }
+}
+
+echo(json_encode($json));
+
+?>
