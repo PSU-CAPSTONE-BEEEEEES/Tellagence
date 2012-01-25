a2abe081fe3fd10009abffe47d32c625d407ed23
Jesusaurus <jesusaurus@inbox.com>
2012-01-25 00:43:58 -0800

    It will currently insert 100 nodes into the database.  The random.php script fails with sizes > 1720, it needs to updated with an offset variable.

--- research/phpsql/propagate.php
+++ research/phpsql/propagate.php
@@ -6,16 +6,18 @@ if (!$dbconn) {
     die("Error connecting to database.");
 }
 
-$string = file_get_contents("http://capstone06.cs.pdx.edu/data/random.php");
+$string = file_get_contents("http://capstone06.cs.pdx.edu/data/random.php?size=100");
 $json = json_decode($string,true);
 
 echo(json_encode($json));
 
 foreach ($json['nodes'] as $node) {
     $source = $node['name'];
+    pg_Exec($dbconn, "INSERT INTO users VALUES ($source, to_char($source,'9999'))");
     foreach ($node['influence'] as $t) {
 	foreach ($t as $target => $inf ) {
 	    echo($source . " " . $target . " " . $inf . "<br \>");
+	    pg_Exec($dbconn, "INSERT INTO relationship VALUES ($source, $target, $inf, $inf)");
 	}
     }
 }
