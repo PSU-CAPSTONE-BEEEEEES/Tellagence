37d50aa7eadf30c6a110b1c6bf82e517eb4964e9
Jesusaurus <jesusaurus@inbox.com>
2012-01-24 22:49:55 -0800

    Tearing apart (read: understanding) the JSON.

--- research/phpsql/propagate.php
+++ research/phpsql/propagate.php
@@ -1,13 +1,21 @@
 <?php
 //propagate the test table with a lot of random data
 
-$dbconn = pg_Connect("host=capstone06.cs.pdx.edu dbname=fake user=postgres password=bees")
+$dbconn = pg_Connect("host=capstone06.cs.pdx.edu dbname=fake user=postgres password=bees");
 if (!$dbconn) {
     die("Error connecting to database.");
 }
 
-$string = file_get_contents("/data/random.php?size=5");
-$json = json_decode($string);
+$string = file_get_contents("http://capstone06.cs.pdx.edu/data/random.php");
+$json = json_decode($string,true);
 
-echo($json);
+echo(json_encode($json));
+
+foreach ($json['nodes'] as $source) {
+    foreach ($source['influence'] as $target) {
+	foreach ($target as $i) {
+	    echo($source['name'] . " " . $target . " " . $target[$i] . "<br \>");
+	}
+    }
+}
 ?>
