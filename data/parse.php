c177989a56647f2664a2cb9e708c3ef754208f6f
Tien Le <physicaltalent@yahoo.com>
2012-01-27 18:46:41 -0800

    delete 3 files, change parse

--- data/parse.php
+++ data/parse.php
@@ -4,8 +4,9 @@
 
 include 'simplexlsx/simplexlsx.class.php';
 ini_set('max_execution_time', 0);
+ini_set('memory_limit', '-1');
     
-    $xlsx = new SimpleXLSX('small.xlsx');
+    $xlsx = new SimpleXLSX('medium.xlsx');
     echo '<h1>$xlsx->rows()</h1>';
 
 
@@ -18,45 +19,116 @@ echo '<pre>';
 
 // move to DB
 /*
-$dbconn = pg_connect("host=capstone06.cs.pdx.edu dbname=vmworld user=root password=");
+$dbconn = pg_connect("host=capstone06.cs.pdx.edu dbname=vmworld user=postgres password=bees");
 if (!$dbconn) {
      die("Error in connection: " . pg_last_error());
  }
 echo 'hahah';die;
 */
     // usernames & tweets
+    $ary = array();
     for($i = 1; $i <= $num_rows; $i++)
     {
-        $active = $a[$i][2];
-        $node = array(
-            'name' => $active,
-            //'influence' =>
-        );
 
-        print_r($active);
-        print_r('<br \>');
-        //print_r($a[$i][4]);
-        for ($j = 1; $j <= $num_rows; $j++)
-        {
+        if (strpos($a[$i][4], '@') !== false){
             $str = $a[$i][4];
-            if(stristr($str,"@")){
-               $str1 = $a[$i][4];
-               $str2 = strstr ($str1, "@");
-               $word = explode(" ", $str2);
-               $word[0] = rtrim($word[0], ":");
-               $word[0] = str_replace("@","",$word[0]);
+        // $str = full tweet contains @
+            // RT
+            if(strpos($str, 'RT') !== false){
+                $str3 = strstr($str, "RT @");
+                $word3 = explode(" ", $str3);
+                $word3[1] = rtrim($word3[1], ":");
+                $word3[1] = rtrim($word3[1], ",");
+                $word3[1] = str_replace("@","",$word3[1]);
+                // revert Source and Destination
+                if($word3[1]){
+                    $username[] = str_replace("@","",$word3[1]);
+                    $ary[] = $a[$i][2];
+                }
+            }
+            // @
+            else{
+                $str2 = strstr($str, "@");
+                // str2 is substring of str from @
+                $word = explode(" ", $str2);
+                $word[0] = rtrim($word[0], ":");
+                $word[0] = rtrim($word[0], ",");
+                $word[0] = rtrim($word[0], ".");
+                $word[0] = str_replace("@","",$word[0]);
+                if($word[0]){
+                    $ary[] = $word[0];
+                    $username[] = $a[$i][2];
+                }
+
             }
 
         }
-        print_r($word[0]);
-        print_r('<br \>');
+    }
+    // $ary3 is array of username that come after RT @
+    //print_r($ary3);
+    // $ary is array of username that come after @ or RT @
+    //print_r($ary);
+
+    for ($i = 0; $i < count($username); $i++){
+        $relation[] = array(
+            'source' => $username[$i],
+            'target' => $ary[$i]
+        );
     }
 
-    //print_r( $xlsx->rows() );
+//print_r(array_unique($username));die;
+// create purpose array
+foreach ($relation as $item){
+    $purpose[] = $item['source'].'*-*'.$item['target'];
+}
+
+$count = array_count_values($purpose);
+
+$result = array();
+    foreach ($count as $key=>$value) {
+        $temp = explode('*-*', $key);
+        $result[] = array(
+            'source' => $temp[0],
+            'target' => $temp[1],
+            'inf' => $value
+        );
+    }
+print_r($result); 
 
 
-echo '</pre>';    
 
+//begin JSON part
+/*
+
+$json['nodes'] = array();
+for ($i = 0; $i <= $count; $i++) {
+    $node['name'] = genName($i);
+    $json['nodes'][] = $node;
+}
+
+$json['links'] = array();
+for ($i = 0; $i <= $count; $i++) {
+    for ($j = $i-1; $j > 0; $j--) {
+        if ( rand(0,$sparse) == 0 ) {
+            $link['source'] = $i;
+            $link['target'] = $j;
+            // count it
+            $link['influence'] = rand($min,$max);
+	    $json['links'][] = $link;
+        }
+    }
+}
+
+echo(json_encode($json));
+*/
+
+
+function array_unique_multidimensional($input)
+{
+    $serialized = array_map('serialize', $input);
+    $unique = array_unique($serialized);
+    return array_intersect_key($input, $unique);
+}
 
 ?>
 
