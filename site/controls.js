450b196f87649a9bf28d2707cd8643e2c1b44f3c
Derek Hurley <dzhurley@gmail.com>
2012-01-31 14:18:40 -0800

    added help overlay and d3 credit to toolbar
    a link to the d3.js site gives credit to the
    library we are using, and the pop up help from
    the '?' allows users to read a (soon to be)
    simple user manual for the application
    
    to complete the ticket, we need a search bar
    and zoom slider in the bar

--- site/controls.js
+++ site/controls.js
@@ -126,9 +126,63 @@ function zoomSlider(val)
     console.log("zoomSlider called with value: " + val);
 }
 
+
+/* ******************************************
+   toolbar animation and help overlay functions
+*/
+
 $("#toolbar").hide();
-$(".toggle").click(function(){
-    $("#toolbar").slideToggle("fast");
-    return false;
+$(".over").hover(
+    function(){
+        $("#toolbar").slideToggle("slow");
+    },
+    function(){
+        $("#toolbar").slideToggle("slow");
+    }
+);
+
+function loadPopup(){
+//loads popup only if it is disabled
+    if($("#bgPopup").data("state")==0){
+        $("#bgPopup").css({
+            "opacity": "0.7"
+        });
+        $("#bgPopup").fadeIn("medium");
+        $("#Popup").fadeIn("medium");
+        $("#bgPopup").data("state",1);
+    }
+}
+
+function disablePopup(){
+    if ($("#bgPopup").data("state")==1){
+        $("#bgPopup").fadeOut("medium");
+        $("#Popup").fadeOut("medium");
+        $("#bgPopup").data("state",0);
+    }
+}
+
+function centerPopup(){
+    var winw = $(window).width();
+    var winh = $(window).height();
+    var popw = $('#Popup').width();
+    var poph = $('#Popup').height();
+    $("#Popup").css({
+        "position" : "absolute",
+        "top" : winh/2 - poph/2,
+        "left" : winw/2 - popw/2
+    });
+}
+
+$("#bgPopup").data("state",0);
+$("#help_button").click(function(){
+    centerPopup();
+    loadPopup();
+});
+$("#bgPopup").click(function(){
+    disablePopup();
+});
+
+$(window).resize(function() {
+    centerPopup();
 });
 });
