0a6e669423c13a9b6ef1e5490aaec6c5acbc2f22
K Jonathan Harker <kjharke@cs.pdx.edu>
2012-01-30 17:12:31 -0800

    Abstract the user we are looking for.

--- research/phpsql/search.php
+++ research/phpsql/search.php
@@ -1,21 +1,39 @@
 <?php
-//propagate the test table with a lot of random data
+//Test searching the fake db
+
+$user = 1;
+if (isset($_GET["id"])) {
+  $id = $_GET["id"];
+
+  if ( (int)$id == $id && $id >= 0 ) {
+    $user = (int)$id;
+  }
+}
 
 $dbconn = pg_Connect("host=capstone06.cs.pdx.edu dbname=fake user=postgres password=bees");
 if (!$dbconn) {
     die("Error connecting to database.");
 }
 
-$result = pg_Exec($dbconn, "SELECT * FROM users WHERE user_id = 200;");
+$result = pg_Exec($dbconn, "SELECT * FROM users WHERE user_id = $user;");
 $num = pg_numrows($result);
 for ($i = 0; $i < $num; $i++) {
-    echo(pg_fetch_array($result, $i) . "<br />");
+    $row = pg_fetch_array($result, $i);
+    print($row[0] . " " . $row[1] . "<br />");
 }
 
-$result = pg_Exec($dbconn, "SELECT * FROM relationship WHERE user_id1 = 200;");
+$result = pg_Exec($dbconn, "SELECT user_id2, inf_1to2, inf_2to1 FROM relationship WHERE user_id1 = $user;");
 $num = pg_numrows($result);
 for ($i = 0; $i < $num; $i++) {
-    echo(pg_fetch_array($result, $i) . "<br />");
+    $row = pg_fetch_array($result, $i);
+    print($row[1] . ": " . $row[2] . "<br />");
+}
+
+$result = pg_Exec($dbconn, "SELECT user_id1, inf_1to2, inf_2to1 FROM relationship WHERE user_id2 = $user;");
+$num = pg_numrows($result);
+for ($i = 0; $i < $num; $i++) {
+    $row = pg_fetch_array($result, $i);
+    print($row[1] . ": " . $row[2] . "<br />");
 }
 
 pg_close($dbconn);
