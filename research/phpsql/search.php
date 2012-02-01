a06ed11baf71da09ef35fd0010ab58c7e6b871a0
K Jonathan Harker <kjharke@cs.pdx.edu>
2012-02-01 01:11:04 -0800

    Added database->json reindexing.
    Now the json links array is reindexed to the json nodes array,
    not the user database.

--- research/phpsql/search.php
+++ research/phpsql/search.php
@@ -22,8 +22,12 @@ if (isset($_GET["depth"])) {
 }
 
 //how many nodes we've visited so far
+//also used to map database index <-> json index
 $visited[] = $user;
+
+//list of nodes to visit
 $toVisit = array();
+
 
 //create the json skeleton
 $json['nodes'] = array();
@@ -59,8 +63,8 @@ while (count($visited) < $total && count($toVisit) > 0) {
 
 foreach ($toVisit as $next) {
     //TODO: some sort of cleanup
-    //maybe addNode but no addLinks
-    //or maybe clear links to these nodes
+    //clear links to these nodes
+
 }
 
 echo(json_encode($json));
@@ -71,9 +75,11 @@ pg_close($dbconn);
 //here be dragons^Wfunctions
 function addNode($who) {
     global $dbconn, $toVisit, $visited, $json;
+
     //find this nodes name
     $result = pg_Exec($dbconn, "SELECT username FROM users WHERE user_id = $who;");
     $num = pg_numrows($result);
+    
     for ($i = 0; $i < $num; $i++) {
         $row = pg_fetch_array($result, $i);
         $node['name'] = $row[0];
@@ -85,48 +91,71 @@ function addNode($who) {
 
 function addLinks($who) {
     global $dbconn, $toVisit, $visited, $json;
+
     //find our immediate links
+    //round 1: user_id1
     $result = pg_Exec($dbconn, "SELECT user_id2, inf_1to2, inf_2to1 FROM relationship WHERE user_id1 = $who;");
     $num = pg_numrows($result);
+    
     for ($i = 0; $i < $num; $i++) {
         $row = pg_fetch_array($result, $i);
-        $toVisit[] = $row[0];
+        
+        //map index into the json array
+        if (!in_array($row[0],$visited)) {
+            $toVisit[] = $row[0];
+            $goal = count($visited) + count($toVisit) - 1;
+        } else {
+            $goal = array_search($row[0],$visited);
+        }
+
         //build the links
         if ($row[1] > 0) {
-            $link['source'] = $who;
-            $link['target'] = $row[0];
+            $link['source'] = count($json['nodes']) - 1;
+            $link['target'] = $goal;
             $link['influence'] = $row[1];
             $json['links'][] = $link;
         }
+
+        //build the links
         if ($row[2] > 0) {
-            $link['source'] = $row[0];
-            $link['target'] = $who;
+            $link['source'] = $goal;
+            $link['target'] = count($json['nodes']) - 1;
             $link['influence'] = $row[2];
             $json['links'][] = $link;
         }
-        //TODO: reindex source/target to the nodes array
     }
 
     //find our immediate links
+    //round 2: user_id2
     $result = pg_Exec($dbconn, "SELECT user_id1, inf_1to2, inf_2to1 FROM relationship WHERE user_id2 = $who;");
     $num = pg_numrows($result);
+
     for ($i = 0; $i < $num; $i++) {
         $row = pg_fetch_array($result, $i);
-        $toVisit[] = $row[0];
+    
+        //map index into the json array
+        if (!in_array($row[0],$visited)) {
+            $toVisit[] = $row[0];
+            $goal = count($visited) + count($toVisit) - 1;
+        } else {
+            $goal = array_search($row[0],$visited);
+        }
+
         //build the links
         if ($row[1] > 0) {
-            $link['source'] = $who;
-            $link['target'] = $row[0];
+            $link['source'] = count($json['nodes']) - 1;
+            $link['target'] = $goal;
             $link['influence'] = $row[1];
             $json['links'][] = $link;
         }
+    
+        //build the links
         if ($row[2] > 0) {
-            $link['source'] = $row[0];
-            $link['target'] = $who;
+            $link['source'] = $goal;
+            $link['target'] = count($json['nodes']) - 1;
             $link['influence'] = $row[2];
             $json['links'][] = $link;
         }
-        //TODO: reindex source/target to the nodes array
     }
 
 }
