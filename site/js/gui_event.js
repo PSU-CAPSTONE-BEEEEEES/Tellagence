e28770ae844c6413882b1e33993f33e759064e22
Derek Hurley <dzhurley@gmail.com>
2012-02-04 16:56:08 -0800

    finished up work on merging controls.js fixed search input catch added alt text to menu tab

--- site/js/gui_event.js
+++ site/js/gui_event.js
@@ -1,22 +1,68 @@
 function GuiEvent(graphRender) {
-	// graph render for this gui event
-	this.graphRender = graphRender;
-	
-	// on click empty the graph
-	$('input#btn_erase').click(function() {
-		graphRender.erase();
-	});
-	
-	// on click redraw the graph
-	$('input#btn_redraw').click(function() {
-		graphRender.draw();
-	});
-	
-	// on click change size of current graph
-	$('input#btn_redraw_data').click(function() {
-		var size = $('input#nodes').val();
-		graphRender.erase();
-		graphRender.changeData(size);
-		graphRender.draw();
-	});
+    // graph render for this gui event
+    this.graphRender = graphRender;
+    graphRender.draw();
+
+    // extract input capture on enter
+    $("#search").keypress(function(e) {
+        // 13 is the ascii code for the enter key
+        if (e.which == 13) {
+	    alert("enter pressed with " + $("#searchbar").val());
+        }
+    });
+    
+    // toolbar animation and help overlay functions
+    $("#toolbar").hide();
+    $("#tab").click(
+        function(){
+            $("#toolbar").slideToggle("fast");
+        }
+    );
+    
+    // functions for controlling the help overlay visibility and position
+    function loadPopup(){
+        //loads popup only if it is disabled
+        if($("#bgPopup").data("state")==0){
+            $("#bgPopup").css({
+                "opacity": "0.7"
+            });
+            $("#bgPopup").fadeIn("medium");
+            $("#Popup").fadeIn("medium");
+            $("#bgPopup").data("state",1);
+        }
+    }
+    
+    function disablePopup(){
+        if ($("#bgPopup").data("state")==1){
+            $("#bgPopup").fadeOut("medium");
+            $("#Popup").fadeOut("medium");
+            $("#bgPopup").data("state",0);
+        }
+    }
+    
+    function centerPopup(){
+        var winw = $(window).width();
+        var winh = $(window).height();
+        var popw = $('#Popup').width();
+        var poph = $('#Popup').height();
+        $("#Popup").css({
+            "position" : "absolute",
+            "top" : winh/2 - poph/2,
+            "left" : winw/2 - popw/2
+        });
+    }
+    
+    // selectors on the help button to trigger the functions
+    $("#bgPopup").data("state",0);
+    $("#help_button").click(function(){
+        centerPopup();
+        loadPopup();
+    });
+    $("#bgPopup").click(function(){
+        disablePopup();
+    });
+    
+    $(window).resize(function() {
+        centerPopup();
+    });
 }
