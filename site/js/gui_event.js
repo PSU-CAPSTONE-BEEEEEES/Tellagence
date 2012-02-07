10004e93aeec8bdd9d17a230c8e31f2515b663cc
aren <aren@pdx.edu>
2012-02-06 16:29:55 -0800

    added in my slider zoom

--- site/js/gui_event.js
+++ site/js/gui_event.js
@@ -10,7 +10,33 @@ function GuiEvent(graphRender) {
 	    alert("enter pressed with " + $("#searchbar").val());
         }
     });
-    
+
+    var initial
+    $("#slider").mousedown(function () {
+        initial = this.value;
+    }).bind("mouseup mouseleave", function () {
+        //alert(this.value - initial);
+        var diff = this.value - initial;
+        if (diff < 0) {
+            ChromeWheel( 1 , Math.abs(diff) / 5);
+        }
+        else {
+            ChromeWheel( 0 , diff / 5);
+        }
+    });
+
+    var view
+    window.onload = function () {
+        view = document.getElementById("inner");
+    }
+
+    function ChromeWheel ( shift_key , clicks) {
+        var evt = document.createEvent("MouseEvents");
+        evt.initMouseEvent('dblclick', true, true, window, clicks, 10, 10,
+        $(window).width() / 2, $(window).height() / 2, 0, 0, shift_key, 0, 1, null);
+        view.dispatchEvent(evt);
+    }
+        
     // toolbar animation and help overlay functions
     $("#toolbar").hide();
     $("#tab").click(
