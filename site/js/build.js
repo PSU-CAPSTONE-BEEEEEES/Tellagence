787aada74b53493dd16712116be428ff6d3be7eb
longuyen <longuyen@cecs.pdx.edu>
2012-02-21 17:34:38 -0800

    working on subgraph integration

--- site/js/build.js
+++ site/js/build.js
@@ -1,8 +1,11 @@
 d3.json("data/subgraph.php", function(data) {
 	// switch the spinning bar for the loading bar
 	switchBars();
-	// render for this graph
-	var sgr = new SubgraphRender(data.graphs);
+	// render for this object
+	var render = new SubgraphRender(data.graphs);
 	// draw subgraph (w/ graph events ready)
-	sgr.draw();
+	render.draw();
+	
+	// gui event for subgraphs
+	var ge = new GuiEvent(render);
 });
