c974d9dc03ab74da8807dd83252f78ff744f6dbd
Derek Hurley <dzhurley@gmail.com>
2012-03-09 23:06:51 -0800

    nearly fixed initial zoom issues
    the problem was when the render was panned around,
    then the initZoom used a center dblclick action to
    zoom in, pushing the graph off the screen
    
    the approach now is to center the graph based on the
    farthest top, bottom, left, and right nodes positions,
    and all is in place aside from some logic updating
    the translate arguments
    
    this is a work in progress commit

--- site/js/site_gui_event.js
+++ site/js/site_gui_event.js
@@ -1,3 +1,79 @@
+function getNode(node) {
+    // translate a d3 node into a jquery selected element
+    var contains = node.subgraph_id !== undefined ?
+            "id="+node.subgraph_id+" num="+node.num :
+            'UserId='+node.id+'UserName='+node.name;
+    return $("title:contains("+contains+")").parent();
+}
+
+function centerGraph(nodes, w, h) {
+    // find nodes at farthest right, left, top, and bottom
+    var tNode, bNode, rNode, lNode;
+    for (var i = 0; i < nodes.length; i++) {
+        var top  = getNode(nodes[i]).position().top;
+        var left = getNode(nodes[i]).position().left;
+
+        if ((tNode === undefined) || (top < tNode.position().top)) {
+            tNode = getNode(nodes[i]);
+        }
+        if ((bNode === undefined) || (top > bNode.position().top)) {
+            bNode = getNode(nodes[i]);
+        }
+        if ((lNode === undefined) || (left < lNode.position().left)) {
+            lNode = getNode(nodes[i]);
+        }
+        if ((rNode === undefined) || (left > rNode.position().left)) {
+            rNode = getNode(nodes[i]);
+        }
+    }
+
+    // within 5 pixels seems reasonable
+    function hCentered() {
+        return Math.abs(tNode.position().top - (h - bNode.position().top)) < 20;
+    };
+    function wCentered() {
+        return Math.abs(lNode.position().left - (w - rNode.position().left)) < 20;
+    };
+
+    // translate the graph until centered
+    while ((hCentered() && wCentered()) !== true) {
+        // translate(0, 0) scale(1)
+        var splits = $("#inner").attr("transform").split("(");
+        var transSplit = splits[1].split(")")[0].split(",");
+        var scaleSplit = parseInt(splits[2].split(")")[0]);
+
+        if (transSplit.length == 1) { break; }
+
+        var trans1 = parseInt(transSplit[0]);
+        var trans2 = parseInt(transSplit[1]);
+
+        var newTrans1, newTrans2;
+        if (tNode.position().top < (h - bNode.position().top)) {
+            newTrans1 = trans1 - 20;
+        }
+        else if (tNode.position().top > (h - bNode.position().top)) {
+            newTrans1 = trans1 + 20;
+        }
+        else {
+            newTrans1 = trans1;
+        }
+
+        if (lNode.position().left > (w - rNode.position().left)) {
+            newTrans2 = trans2 - 20;
+        }
+        else if (lNode.position().left < (w - rNode.position().left)){
+            newTrans2 = trans2 + 20;
+        }
+        else {
+            newTrans2 = trans2;
+        }
+
+        d3.select("#inner").attr("transform",
+                                 "translate(" + newTrans1 + ", " + newTrans2 + ")"
+                                 + " scale(" + scaleSplit + ")");
+    }
+}
+
 function initZoom(render) {
     // initialize the render to a decent zoom level
     var w = $(window).width(),
@@ -5,10 +81,7 @@ function initZoom(render) {
 
     function graphCheck(node, i, array) {
         // find all the nodes and filter for the ones off screen
-        var contains = node.subgraph_id !== undefined ?
-                "id="+node.subgraph_id+" num="+node.num :
-                'UserId='+node.id+'UserName='+node.name;
-        var circle = $("title:contains("+contains+")").parent();
+        var circle = getNode(node);
         var cx = circle.position().left;
         var cy = circle.position().top;
         if ((cx < 0) || (cx > w)) { return true; }    // check x
@@ -17,8 +90,12 @@ function initZoom(render) {
     }
 
     // subgraphs and graphs have different names for nodes
+    var nodes = render.graphs !== undefined ? render.graphs : render.nodes;
     var offNodes = render.graphs !== undefined ?
             render.graphs.filter(graphCheck) : render.nodes.filter(graphCheck);
+
+    // recenter graph
+    centerGraph(nodes, w, h);
 
     // zoom in first if we can
     while (offNodes.length === 0) {
