634ff6c1456edeb9027674320969821bd557656b
Tien Le <letien@pdx.edu>
2012-03-19 13:31:01 -0700

    fix indentation in php files

--- data/fix.php
+++ data/fix.php
@@ -1,4 +1,8 @@
 <?php
+
+// this file makes user_id list continuous in the users table --> enhance the performance
+// update 2 table: users and relationship
+
 function cmp($a, $b)
 {
     if ($a == $b) {
@@ -8,49 +12,47 @@ function cmp($a, $b)
 }
 
 
-
-
 // down & flood realusers table
-    $dbcon = pg_connect("host=capstone06.cs.pdx.edu dbname=real user=postgres password=bees");
-    if (!$dbcon) {
-         die("Error in connection: " . pg_last_error());
-    }
-    // execute query
-    $sql = "SELECT user_id FROM users";
-    $result = pg_query($dbcon, $sql);
+$dbcon = pg_connect("host=capstone06.cs.pdx.edu dbname=real user=postgres password=bees");
+if (!$dbcon) {
+     die("Error in connection: " . pg_last_error());
+}
+// execute query
+$sql = "SELECT user_id FROM users";
+$result = pg_query($dbcon, $sql);
 
-    $row = array();
-    for ($i=0;$i < pg_num_rows($result); $i++) {
-            $row = pg_fetch_array($result,$i);
-            $inspector[] = $row[0];
-    }
-    // free memory
-    pg_free_result($result);
+$row = array();
+for ($i=0;$i < pg_num_rows($result); $i++) {
+        $row = pg_fetch_array($result,$i);
+        $inspector[] = $row[0];
+}
+// free memory
+pg_free_result($result);
 
-    usort($inspector, "cmp");
-    print_r($inspector);
+usort($inspector, "cmp");
+print_r($inspector);
 
-    foreach($inspector as $key=>$value){
-        $old = $value;
-        $new = $key + 1;
-        if($new != $old){
-        $sql0 = "UPDATE users SET user_id = ('$new') WHERE user_id = ('$old')";
-        $sql1 = "UPDATE relationship SET user_id1 = ('$new') WHERE user_id1 = ('$old')";
-        $sql2 = "UPDATE relationship SET user_id2 = ('$new') WHERE user_id2 = ('$old')";
-        $result0 = pg_query($dbcon, $sql0);
-        $result1 = pg_query($dbcon, $sql1);
-        $result2 = pg_query($dbcon, $sql2);
-            if (!$result0 || !$result1 || !$result2) {
-             die("Error in SQL query: " . pg_last_error());
-            }
-            echo "Good update";
+foreach($inspector as $key=>$value){
+    $old = $value;
+    $new = $key + 1;
+    if($new != $old){
+    $sql0 = "UPDATE users SET user_id = ('$new') WHERE user_id = ('$old')";
+    $sql1 = "UPDATE relationship SET user_id1 = ('$new') WHERE user_id1 = ('$old')";
+    $sql2 = "UPDATE relationship SET user_id2 = ('$new') WHERE user_id2 = ('$old')";
+    $result0 = pg_query($dbcon, $sql0);
+    $result1 = pg_query($dbcon, $sql1);
+    $result2 = pg_query($dbcon, $sql2);
+        if (!$result0 || !$result1 || !$result2) {
+         die("Error in SQL query: " . pg_last_error());
         }
+        echo "Good update";
     }
-    // free memory
-    pg_free_result($result0);
-    pg_free_result($result1);
-    pg_free_result($result2);
-    // close connection
-    pg_close($dbcon);
+}
+// free memory
+pg_free_result($result0);
+pg_free_result($result1);
+pg_free_result($result2);
+// close connection
+pg_close($dbcon);
 
 ?>
--- data/parse.php
+++ data/parse.php
@@ -1,9 +1,7 @@
 <?php
-/*
-$string = 'RT @username: lorem ipsum @cjoudrey @longnguyen,@letien etc...';
-preg_match_all('/@([A-Za-z0-9_]+)/', $string, $usernames);
-print_r($usernames);die;
-*/
+
+//this file parse the @username form the tweet and count how many times each user talk to each other.
+
 
 include 'simplexlsx/simplexlsx.class.php';
 ini_set('max_execution_time', 0);
@@ -11,11 +9,12 @@ ini_set('memory_limit', '-1');
 
 
 
-
+// sanitize the username from the tweet
 function san($str){
     preg_match_all('/@([A-Za-z0-9-_]+)/', $str, $usernames);
     return $usernames[1];
 }
+// check time inside the tweet (eg. @11.30)
 function TimeInside($str){
     $str2 = strstr($str, "@");
     // str2 is substring of str from @
@@ -24,9 +23,48 @@ function TimeInside($str){
     return preg_match("/([0-9]+)(:|.)([0-9]+)/i",$old);
 }
 
+function removeDuplication($e1, $e2){
+    $purpose1 = array();
+    $purpose2 = array();
+    
+    foreach ($e1 as $item){
+        $purpose1[] = $item['user_id1'].'*-*'.$item['user_id2'];
+    }
+    $count1 = array_count_values($purpose1);
+
+    foreach ($e2 as $item){
+        $purpose2[] = $item['user_id1'].'*-*'.$item['user_id2'];
+
+    }
+    $count2 = array_count_values($purpose2);
+
+    $res = array();
+    foreach($count1 as $key=>$value){
+        if(isset($count2[$key])){
+            $temp = explode('*-*', $key);
+            $res[] = array(
+                'user_id1' => $temp[0],
+                'user_id2' => $temp[1],
+                'inf_1to2' => $value,
+                'inf_2to1' => $count2[$key],
+            );
+        }else{
+            $temp = explode('*-*', $key);
+            $res[] = array(
+                'user_id1' => $temp[0],
+                'user_id2' => $temp[1],
+                'inf_1to2' => $value,
+                'inf_2to1' => 0,
+            );
+        }
+    }
+    return $res;
+}
+
+
 function main(){
 
-    $xlsx = new SimpleXLSX('simplexlsx/vmworld.xlsx');
+    $xlsx = new SimpleXLSX('verysmall.xlsx');
     list($num_cols, $num_rows) = $xlsx->dimension();
 
     $a = $xlsx->rows();
@@ -77,9 +115,9 @@ function main(){
     }
     $unique_total = array_merge(array_unique(array_merge($username, $ary)));
 
-    // flood DB
-
-    $dbcon = pg_connect("host=capstone06.cs.pdx.edu dbname=real user=postgres password=bees");
+    // flood DB with username and user_id
+    /*
+    $dbcon = pg_connect("host=capstone06.cs.pdx.edu dbname=vmworld user=postgres password=bees");
     if (!$dbcon) {
          die("Error in connection: " . pg_last_error());
     }
@@ -98,13 +136,7 @@ function main(){
     echo "END: Users successfully inserted!";
     // close connection
     pg_close($dbcon);
-
-
-
-    //print_r($unique_total);
-    //print_r(count($unique_total));
-    //print_r($relation);
-
+    */
     // replace username with their user_id
     // and re-order + regroup
     foreach($relation as &$item){
@@ -122,62 +154,58 @@ function main(){
                 'user_id2' => $index + 1,
                 );
             }
-        }
-    }
-
-    // remove duplication
-    foreach ($e1 as $item){
-        $purpose1[] = $item['user_id1'].'*-*'.$item['user_id2'];
-    }
-    $count1 = array_count_values($purpose1);
-
-    foreach ($e2 as $item){
-        $purpose2[] = $item['user_id1'].'*-*'.$item['user_id2'];
-
-    }
-    $count2 = array_count_values($purpose2);
-
-
-    foreach($count1 as $key=>$value){
-        if(isset($count2[$key])){
-            $temp = explode('*-*', $key);
-            $res[] = array(
-                'user_id1' => $temp[0],
-                'user_id2' => $temp[1],
-                'inf_1to2' => $value,
-                'inf_2to1' => $count2[$key],
-            );
-        }else{
-            $temp = explode('*-*', $key);
-            $res[] = array(
-                'user_id1' => $temp[0],
-                'user_id2' => $temp[1],
-                'inf_1to2' => $value,
-                'inf_2to1' => 0,
-            );
 
         }
     }
-    //print_r($res);
 
+    // remove&count duplication
+    $res = removeDuplication($e1, $e2);
 
-    foreach($res as $item){
-        $source[] = $item['user_id1'];
-        $target[] = $item['user_id2'];
-    }
-
-
-
-    $cs = array_count_values($source);
-    $ct = array_count_values($target);
-
-    // flood database 3
+     // flood database 2
+    // flood table relationship with full data
     /*
     $dbcon = pg_connect("host=capstone06.cs.pdx.edu dbname=vmworld user=postgres password=bees");
     if (!$dbcon) {
          die("Error in connection: " . pg_last_error());
     }
+    // execute query
+    foreach($res as $item){
+        $u1 = $item['user_id1'];
+        $u2 = $item['user_id2'];
+        $x = $item['inf_1to2'];
+        $y = $item['inf_2to1'];
+        $z = $x + $y;
+        $sql = "INSERT INTO relationship (user_id1, user_id2, inf_1to2, inf_2to1, inf_sum) VALUES('$u1','$u2','$x','$y','$z')";
+        $result = pg_query($dbcon, $sql);
 
+        if (!$result) {
+         die("Error in SQL query: " . pg_last_error());
+        }
+        echo "Data successfully inserted!";
+    }
+    // free memory
+    pg_free_result($result);
+
+    // close connection
+    pg_close($dbcon);
+    */
+    
+    // calc node degree
+    foreach($res as $item){
+        $source[] = $item['user_id1'];
+        $target[] = $item['user_id2'];
+    }
+
+    $cs = array_count_values($source);
+    $ct = array_count_values($target);
+
+    // flood database 3
+    // update table users with degree
+    /*
+    $dbcon = pg_connect("host=capstone06.cs.pdx.edu dbname=vmworld user=postgres password=bees");
+    if (!$dbcon) {
+         die("Error in connection: " . pg_last_error());
+    }
     // execute query for out_degree
     foreach($cs as $key=>$value){
         $sql = "UPDATE users SET out_degree = ('$value') WHERE user_id = ('$key')";
@@ -208,11 +236,11 @@ function main(){
         if (!$result) {
          die("Error in SQL query: " . pg_last_error());
         }
-        echo "Sum-degree successfully updated!";
+    echo "Sum-degree successfully updated!";
     // free memory
     pg_free_result($result);
 
-    // remove redundant nodes 695 nodes
+    // remove redundant nodes (695 nodes) with sum_degree = 0
     $sql = "DELETE FROM users WHERE sum_degree = 0";
     $result = pg_query($dbcon, $sql);
         if (!$result) {
@@ -222,213 +250,17 @@ function main(){
     // free memory
     pg_free_result($result);
 
-
     // close connection
     pg_close($dbcon);
-
     */
 
-    // down & flood realusers table
-    $dbcon = pg_connect("host=capstone06.cs.pdx.edu dbname=vmworld user=postgres password=bees");
-    if (!$dbcon) {
-         die("Error in connection: " . pg_last_error());
-    }
-    // execute query
-    $sql = "SELECT * FROM users";
-    $result = pg_query($dbcon, $sql);
-    while($row = mysql_fetch_array($sql))
-    {
-        $inspector[] = $row['user_id'];
-    }
-    if (!$result) {
-     die("Error in SQL query: " . pg_last_error());
-    }
-    echo "Data successfully downloaded!";
+    $unique_total = array_merge(array_unique(array_merge($source, $target)));
+   
 
-    // free memory
-    pg_free_result($result);
-
-    // close connection
-    pg_close($dbcon);
-
-    print_r($inspector);die;
-
-
-    /*
-    // swap order
-    foreach($last as &$item){
-        if($item['user_id1'] < $item['user_id2']){
-        // do nothing
-        }else{
-            // revert
-            $t = $item['user_id1'];
-            $item['user_id1'] = $item['user_id2'];
-            $item['user_id2'] = $t;
-            $v = $item['inf1to2'];
-            $item['inf1to2'] = $item['inf2to1'];
-            $item['inf2to1'] = $v;
-        }
-    }
-    print_r($last);
-    */
-
-
-
-
-    // remove duplication
-    /*
-    foreach ($last as &$item){
-        $purpose[] = array(
-            $item['user_id1'].'*-*'.$item['user_id2'] => $item['inf1to2'].'*---*'.$item['inf2to1'],
-        );
-    }
-    //$count = array_count_values($purpose);
-    print_r($purpose);
-
-
-    foreach($purpose as $item){
-
-    }
-
-    /*
-    $result = array();
-
-    //foreach($purpose as $item)
-
-
-    foreach($purpose as $key=>$value) {
-        $temp = explode('*-*', $value);
-        if($temp[0] == $temp[1]){
-            $result[] = array(
-                'user_id1' => $temp[0],
-                'user_id2' => $temp[1],
-                'sum' => $value
-                );
-        }
-
-
-    }
-    // $result is array without duplication in relationship
-    print_r($result);die;
-
-
-
-
-    //$unique_total = array_merge(array_unique(array_merge($source, $target)));
-    //print_r($unique_total);
-    //print_r(count($unique_total));die;
-    //print_r($unique_total);
-
-    // $last is array with user_id from $result
-
-
-    */
-     // remove duplication
-    /*
-    foreach ($last as &$item){
-        $purp[] = $item['user_id1'].'*-*'.$item['user_id2'];
-    }
-    $co = array_count_values($purp);
-    print_r($purp);
-    print_r($co);die;
-    $result = array();
-    foreach($co as $key=>$value) {
-        $tp = explode('*-*', $key);
-        if($tp[0] != $tp[1]){
-            $out[] = array(
-            'user_id1' => $temp[0],
-            'user_id2' => $temp[1],
-            'inf1to2' => $value
-            );
-        $source[] = $temp[0];
-        $target[] = $temp[1];
-        }
-    }
-    print_r($last);
-    */
 }
-
 
 main();
 
 
-
-//begin JSON part
-/*
-
-$json['nodes'] = array();
-for ($i = 0; $i <= $count; $i++) {
-    $node['name'] = genName($i);
-    $json['nodes'][] = $node;
-}
-
-$json['links'] = array();
-for ($i = 0; $i <= $count; $i++) {
-    for ($j = $i-1; $j > 0; $j--) {
-        if ( rand(0,$sparse) == 0 ) {
-            $link['source'] = $i;
-            $link['target'] = $j;
-            // count it
-            $link['influence'] = rand($min,$max);
-	    $json['links'][] = $link;
-        }
-    }
-}
-
-echo(json_encode($json));
-*/
-
-// flood database
-/*
-    $dbcon = pg_connect("host=capstone06.cs.pdx.edu dbname=vmworld user=postgres password=bees");
-    if (!$dbcon) {
-         die("Error in connection: " . pg_last_error());
-    }
-    die;
-    // execute query
-    for($i = 0; $i < count($unique_total); $i++){
-        $t = $i + 1;
-        $sql = "INSERT INTO users (user_id, username) VALUES('$t' , '$unique_total[$i]')";
-    $result = pg_query($dbcon, $sql);
-    if (!$result) {
-     die("Error in SQL query: " . pg_last_error());
-    }
-    echo "Data successfully inserted!";
-    }
-    // free memory
-    pg_free_result($result);
-
-    // close connection
-    pg_close($dbcon);
-*/
-
-
-// flood database 2
-/*
-    $dbcon = pg_connect("host=capstone06.cs.pdx.edu dbname=vmworld user=postgres password=bees");
-    if (!$dbcon) {
-         die("Error in connection: " . pg_last_error());
-    }
-    // execute query
-    foreach($result as $item){
-        $u1 = $item['user_id1'];
-        $u2 = $item['user_id2'];
-        $x = $item['inf_1to2'];
-        $y = $item['inf_2to1'];
-        $z = $x + $y;
-        $sql = "INSERT INTO relationship (user_id1, user_id2, inf_1to2, inf_2to1, inf_sum) VALUES('$u1','$u2','$x','$y','$z')";
-        $result = pg_query($dbcon, $sql);
-
-        if (!$result) {
-         die("Error in SQL query: " . pg_last_error());
-        }
-        echo "Data successfully inserted!";
-    }
-    // free memory
-    pg_free_result($result);
-
-    // close connection
-    pg_close($dbcon);
-*/
 ?>
 
